0062097102cec6aea2a97d42142ab22e6a318c32
mochisvelas <velasquezmochis@gmail.com>
2020-10-20 22:19:19 -0600

    Added 3 new methods and fixex little typos from crebas

--- procedures.sql
+++ procedures.sql
@@ -17,7 +17,6 @@ begin
 	exec(@bulk_exec)
 end
 ----------------------------------------------------------------
-
 --Trigger to create friendship relations within the new user and
 --all of users that have the same lastname
 create or alter trigger ut_verify_lastname
@@ -56,3 +55,111 @@ as
 			deallocate c_create_friendship
 	end
 ----------------------------------------------------------------
+--Trigger to verify if a friendship already exists,
+--if it does the friendship is not inserted
+create or alter trigger ft_verify_friendship
+on FRIENDSHIP
+instead of insert
+as
+	declare @user1 int
+	declare @user2 int
+	declare @cont int
+
+	select @user1 = USERID, @user2 = FRIENDID from inserted
+
+	select @cont = COUNT(*) from FRIENDSHIP
+	where (USERID = @user1 and FRIENDID = @user2) or (USERID = @user2 and FRIENDID = @user1)
+
+	if (@cont = 0)
+	begin		
+		insert into FRIENDSHIP values (@user1,@user2)
+	end
+
+	else
+	begin
+		print 'El usuario ' + CAST(@user1 as varchar) + ' y el usuario ' +CAST(@user2 as varchar) + ' ya son amigos.'
+	end
+----------------------------------------------------------------
+--Procedure to create a random number of posts
+--for a random number of random users
+create or alter procedure psp_gen_random_posts
+as
+declare @random1 int
+declare @random2 int
+declare @cont1 int
+declare @cont2 int
+declare @total_users int
+declare @userid int
+declare @deviceid int
+begin
+	select @cont1 = 0
+	select @cont2 = 0
+	select @total_users = COUNT(*) from "USER"
+	select @random1 = FLOOR(RAND()*(@total_users))+1;
+	select @random2 = FLOOR(RAND()*(20))+1;
+	print @random1
+	print @random2
+	print @total_users
+
+	while @cont1 != @random1
+	begin
+			select top 1 @userid = USERID from "USER" order by NEWID()
+			
+			while @cont2 != @random2
+			begin
+					begin tran
+					select top 1 @deviceid = DEVICEID from DEVICE order by NEWID()
+					insert into POST values (@userid,1,@deviceid,'255.255.255.255',GETDATE(),'Post generado aleatoriamente :)')
+					select @cont2 = @cont2 + 1
+
+					if @@ERROR <> 0
+					begin
+							print 'No se pudo realizar la transacción.'
+							rollback
+					end
+
+					else
+					begin
+							commit
+					end
+			end
+			select @cont1 = @cont1 + 1
+	end
+end
+----------------------------------------------------------------
+--Procedure to delete a comment
+create or alter procedure csp_delete_comment
+@commentid int
+as
+begin
+	delete from COMMENT where COMMENTID = @commentid
+end
+
+--Trigger to update selected queued comments to active
+create or alter trigger ct_queue_comments
+on COMMENT
+instead of delete
+as
+	declare @newcomment int
+	declare @postid int
+	declare @commentid int
+	declare @total_comments int
+
+	select @commentid = COMMENTID from deleted
+
+	if (@commentid is not NULL)
+	begin
+		select @postid = POSTID from COMMENT where COMMENTID = @commentid
+		delete from COMMENT where COMMENTID = @commentid
+		select top 1 @newcomment = COMMENTID from COMMENT where (status = 0) and POSTID = @postid order by COMMENTDATETIME desc
+
+		if (@newcomment is not NULL)
+		begin
+			update COMMENT set STATUS = 1 where COMMENTID = @newcomment
+		end	
+	end
+
+	else
+	begin
+		print 'El comentario ' + CAST(@commentid as varchar) + ' no existe.'
+	end
