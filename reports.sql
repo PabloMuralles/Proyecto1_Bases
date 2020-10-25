6e34261ded6036a21e5ef9b7e4a3dfcdd75443cf
Pablo Muralles <pablomuralles28@gmail.com>
2020-10-24 22:27:54 -0600

    se arego reporte de los post que llegaron a la cantidad maxima de comentarios

--- reports.sql
+++ reports.sql
@@ -42,3 +42,77 @@ begin
     from INTERACTION
     where (POSTID = @postID) and (INTERACTIONDATETIME between @startDate and @endDate)
 end
+--------------------------------------------------------------------------
+--Reporte de la cantidad de post que llegaron a su cantidad maxima de comentarios en base a un año se desplica cantidad por cada mes del mismo 
+--parametros: @pYear año en el que se desea saber los reportes del mes y si se envia nulo se toma el año en el que se encuentra
+create or alter procedure summary_upPost
+@pYear int
+as
+begin
+	declare @count int
+	set @count = 1;
+	declare @qty int
+
+	drop table if exists SUMMARYPOST;
+
+	create table SUMMARYPOST(Months varchar(20), Quantity int)
+
+	if(@pYear is not null)
+	begin
+		if(LEN(@pYear) = 4)
+		begin
+			while(@count <= 12)
+			begin 
+
+				select P.POSTID
+				into temp123
+				from POST P
+				inner join COMMENT C on P.POSTID = C.POSTID
+				where (MONTH(COMMENTDATETIME) = @count and YEAR(COMMENTDATETIME) =  @pYear )and ACTIVESTATUS = 1
+				group by P.POSTID
+				having COUNT(COMMENTID) = 3
+
+				select @qty = COUNT(1) from temp123
+
+				insert into SUMMARYPOST values(DATENAME(month, DATEADD(month, @count-1, CAST('2008-01-01' AS datetime))), @qty)
+
+				set @count = @count + 1 
+
+				drop table if exists temp123
+
+			end 
+		 select * from SUMMARYPOST
+		end
+		else
+		begin
+			print 'Wrong date format'
+		end
+	end
+	else 
+	begin
+		set @pYear = YEAR(GETDATE())
+		while(@count <= 12)
+			begin 
+
+				select P.POSTID
+				into temp123
+				from POST P
+				inner join COMMENT C on P.POSTID = C.POSTID
+				where (MONTH(COMMENTDATETIME) = @count and YEAR(COMMENTDATETIME) =  @pYear )and ACTIVESTATUS = 1
+				group by P.POSTID
+				having COUNT(COMMENTID) = 3
+
+				select @qty = COUNT(1) from temp123
+
+				insert into SUMMARYPOST values(DATENAME(month, DATEADD(month, @count-1, CAST('2008-01-01' AS datetime))), @qty)
+
+				set @count = @count + 1 
+
+				drop table if exists temp123
+
+
+			end 
+		 select * from SUMMARYPOST
+	end 
+end 
+----------------------------------------------------------------------------------------
